321e6cb8f773cf20af667dd53078a2c929aa1bcb
Qubiz <qubizwow@gmail.com>
2015-05-25 18:35:57 +0200

    ADDED: HUE SeekBar

---
+++ app/src/main/java/qubiz/movinglight/HueSeekBar.java
@@ -0,0 +1,27 @@
+package qubiz.movinglight;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.ColorFilter;
+import android.graphics.drawable.Drawable;
+import android.util.AttributeSet;
+import android.widget.SeekBar;
+
+public class HueSeekBar extends SeekBar {
+
+    public HueSeekBar(Context context) {
+        super(context);
+
+    }
+
+    public HueSeekBar(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public HueSeekBar(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+
+
+}
